b7ae455760a24d25d5305537bc8fdf5a97832b9b
byhooi <byhooi@gmail.com>
2024-05-28 08:04:04 +0800

    Update bsh.js

--- Module/bsh.js
+++ Module/bsh.js
@@ -1,24 +1,26 @@
-// GetToken.js
+// 获取HTTP请求头中的token
 let headers = $request.headers;
 let currentToken = headers['token'];
 
-console.log("当前捕获的Token: " + currentToken); // 输出当前捕获的Token
+// 输出当前捕获的Token
+console.log("当前捕获的Token: " + currentToken);
 
 // 从Surge的持久化存储中读取上一次保存的Token
 let previousToken = $persistentStore.read("token");
 
-console.log("上一次保存的Token: " + previousToken); // 输出上一次保存的Token
+// 输出上一次保存的Token
+console.log("上一次保存的Token: " + previousToken);
 
 // 检查Token是否有变化
 if (currentToken && currentToken !== previousToken) {
     // 如果Token不同，更新存储中的Token，并发送通知
     $persistentStore.write(currentToken, "token");
-    console.log("检测到Token变更，已更新。新Token: " + currentToken); // 输出Token更新信息
+    console.log("检测到Token变更，已更新。新Token: " + currentToken);
     // 发送通知
     $notification.post("Token更新通知", "新的Token已捕获", currentToken);
 } else {
     // 如果Token相同或未捕获到Token，不做任何操作
-    console.log("Token未变更，无需更新。"); // 输出Token未变更信息
+    console.log("Token未变更，无需更新。");
 }
 
 // 结束脚本
